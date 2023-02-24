c62dd825eb07fd483a63ac64cd559172a24a609c
GiancMin <gian.mma@gmail.com>
2023-02-24 16:12:49 -0300

    types and datetime

---
+++ teste.py
@@ -0,0 +1,10 @@
+import datetime
+
+#types
+nome = "Gian"
+idade = 29
+data_nasc = datetime.date(1993,3,3)
+
+print("Variável nome: ", nome,", Tipo da variável nome: ",type(nome))
+print("Variável idade: ", idade,", Tipo da variável idade: ",type(idade)) 
+print("Variável data_basc: ", data_nasc,", Tipo da variável data nasc: ",type(data_nasc)) 
